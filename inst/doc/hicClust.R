9604295dfd42910efd27a49a64d4dd53e1462c7b
Pierre Neuvial <pierre.neuvial@math.univ-toulouse.fr>
2024-10-08 08:10:02 +0000

    version 0.6.10

--- inst/doc/hicClust.R
+++ inst/doc/hicClust.R
@@ -5,34 +5,34 @@ if (!require("HiTC", quietly = TRUE)) {
 }
 
 ## ----loadLib, message=FALSE---------------------------------------------------
-library("adjclust")
+#  library("adjclust")
 
 ## ----loadData-----------------------------------------------------------------
-load(system.file("extdata", "hic_imr90_40_XX.rda", package = "adjclust"))
+#  load(system.file("extdata", "hic_imr90_40_XX.rda", package = "adjclust"))
 
 ## ----create-data-script-------------------------------------------------------
-system.file("system/create_hic_chrXchrX.R", package="adjclust")
+#  system.file("system/create_hic_chrXchrX.R", package="adjclust")
 
 ## ----mapHiC, message=FALSE----------------------------------------------------
-HiTC::mapC(hic_imr90_40_XX)
+#  HiTC::mapC(hic_imr90_40_XX)
 
 ## ----hicClust-HTCexp, message=FALSE-------------------------------------------
-fit <- hicClust(hic_imr90_40_XX)
+#  fit <- hicClust(hic_imr90_40_XX)
 
 ## ----binning, message=FALSE---------------------------------------------------
-binned <- HiTC::binningC(hic_imr90_40_XX, binsize = 1e5)
-fitB <- hicClust(binned)
-fitB
+#  binned <- HiTC::binningC(hic_imr90_40_XX, binsize = 1e5)
+#  fitB <- hicClust(binned)
+#  fitB
 
 ## ----plotBinned, message=FALSE------------------------------------------------
-HiTC::mapC(binned)
+#  HiTC::mapC(binned)
 
 ## ----dendro-------------------------------------------------------------------
-plot(fitB, mode = "corrected")
+#  plot(fitB, mode = "corrected")
 
 ## ----objectDesc---------------------------------------------------------------
-head(cbind(fitB$merge, fitB$gains))
+#  head(cbind(fitB$merge, fitB$gains))
 
 ## ----session------------------------------------------------------------------
-sessionInfo()
+#  sessionInfo()
 
